1cbf02b5b14da100033ae52014de166581cbbe7c
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2016-12-20 19:14:57 +0900

    Move regexes to literals for better readability
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -8,8 +8,11 @@ class Phrase
   end
 
   def word_count
-    @text.downcase.split(/,\s|,|\s/).reduce(Hash.new(0)) do |result_hash, word|
-      cleaned_word = word.gsub(/[\[\]\,!".@#$%^&*();:?-]/, '').gsub(/\A'(.*)'\Z/, '\1')
+    by_comma_and_space = /,\s|,|\s/
+    punctuations = /[\[\]\,!.@#$%^&*();:?-]/
+    surruonding_quotation_marks = /\A'(.*)'\Z/
+    @text.downcase.split(by_comma_and_space).reduce(Hash.new(0)) do |result_hash, word|
+      cleaned_word = word.gsub(punctuations).gsub(surruonding_quotation_marks, '\1')
       result_hash[cleaned_word] += 1
       result_hash
     end
