efdf70a9bc10393b9f3b953a4295a29643b99e8b
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2016-12-20 20:06:23 +0900

    Rename variable @up_to_n to @n and save calculated primes to forgo duplicate calculation
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,24 +1,37 @@
+RubyVM::InstructionSequence.compile_option = {
+  tailcall_optimization: true,
+  trace_instruction: false
+}
+
 module BookKeeping
   VERSION = 1
 end
 
 class Sieve
-  def initialize(up_to_n)
-    @up_to_n = up_to_n
+  def initialize(n)
+    @n = n
+    @primes = nil 
   end
 
-  def primes(target_array = (2..@up_to_n).to_a, result_array = [])
-    if @up_to_n < 2
+  def primes
+    @primes ||= sieve((2..@n).to_a, [])
+  end
+
+  private
+  def sieve(target_array = (2..@n).to_a, result_array = [])
+    if @n < 2
       return result_array
     end
 
     if target_array.size == 1
-      return result_array.concat(target_array)
+      final_result = result_array.concat(target_array)
+      @primes = final_result
+      return final_result
     end
 
     prime = target_array.first
     new_result_array = result_array << prime
     remaining_target_array = target_array.reject { |n| n % prime == 0 }
-    primes(remaining_target_array, new_result_array)
+    sieve(remaining_target_array, new_result_array)
   end
 end
