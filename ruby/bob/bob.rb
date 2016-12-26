819fe364a4f115516c13f6f13396a650b61712de
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2016-12-26 23:26:57 +0900

    Initial commit for Ruby Bob problem
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,22 @@
+class Bob
+  def hey(remark)
+    if is_question?(remark)
+    elsif is_yell?(remark)
+    elsif is_meaningless?(remark)
+    else
+    end
+  end
+
+  private
+  def is_question?(remark)
+
+  end
+
+  def is_yell?(remark)
+
+  end
+
+  def is_meaningless?(remark)
+
+  end
+end
