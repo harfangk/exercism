744ce575fe425bb14e1d9742195f452757d0b70f
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-03 18:10:40 +0900

    Enable tail call optimization in runtime
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,8 +1,3 @@
-RubyVM::InstructionSequence.compile_option = {
-  tailcall_optimization: true,
-  trace_instruction: false
-}
-
 module BookKeeping
   VERSION = 1
 end
@@ -18,20 +13,33 @@ class Sieve
   end
 
   private
-  def sieve(target_array = (2..@n).to_a, result_array = [])
-    if @n < 2
-      return result_array
-    end
 
-    if target_array.size == 1
-      final_result = result_array.concat(target_array)
-      @primes = final_result
-      return final_result
-    end
+  RubyVM::InstructionSequence.compile_option = {
+    tailcall_optimization: true,
+    trace_instruction: false
+  }
 
-    prime = target_array.first
-    new_result_array = result_array << prime
-    remaining_target_array = target_array.reject { |n| n % prime == 0 }
-    sieve(remaining_target_array, new_result_array)
-  end
+  eval <<END
+    def sieve(target_array = (2..@n).to_a, result_array = [])
+      if @n < 2
+        return result_array
+      end
+
+      if target_array.size == 1
+        final_result = result_array.concat(target_array)
+        @primes = final_result
+        return final_result
+      end
+
+      prime = target_array.first
+      new_result_array = result_array << prime
+      remaining_target_array = target_array.reject { |n| n % prime == 0 }
+      sieve(remaining_target_array, new_result_array)
+    end
+  END
 end
+
+RubyVM::InstructionSequence.compile_option = {
+  tailcall_optimization: false,
+  trace_instruction: true
+}
