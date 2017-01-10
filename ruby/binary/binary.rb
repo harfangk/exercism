7b9046fee15ad1fbe4529aec2e9b3a614be57ea4
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-01-10 15:21:23 +0900

    First refactor
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -6,14 +6,20 @@ class Binary
   require 'set'
   
   def self.to_decimal(string)
-    valid_digits = Set.new(['0', '1'])
     result = 0
-    string.each_char.reverse_each.each_with_index do |elem, index|
-      unless valid_digits.include? elem
-        raise ArgumentError
-      end
-      result += elem.to_i * (2 ** index)
+    string.each_char.reverse_each.each_with_index do |number, digit|
+      validate_number(number)
+      result += number.to_i * (2 ** digit)
     end
     result
   end
+
+  def self.validate_number(n)
+    valid_numbers = Set.new(['0', '1'])
+    unless valid_numbers.include? n
+      raise ArgumentError
+    end
+  end
+
+  private_class_method :validate_number
 end
