7b1c257891e93a0b39759fe19039d50b82c309c6
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-04-14 02:44:44 +0900

    Add duplication for readability
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -4,18 +4,19 @@ end
 
 class BeerSong
   def verse(n)
-    first_verse = "#{bottles(n)} of beer on the wall, #{bottles(n)} of beer.".capitalize
-    second_verse = 
-      if n == 0
-        "Go to the store and buy some more, #{bottles(99)} of beer on the wall."
-      else
-        "Take #{correct_pronoun(n)} down and pass it around, #{bottles(n-1)} of beer on the wall."
-      end
-    "#{first_verse}\n#{second_verse}\n"
+    if n != 0
+%Q(#{bottles(n)} of beer on the wall, #{bottles(n)} of beer.
+Take #{correct_pronoun(n)} down and pass it around, #{bottles(n-1)} of beer on the wall.
+)
+    else
+%Q(No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, #{bottles(99)} of beer on the wall.
+)
+    end
   end
 
   def verses(beginning_verse, ending_verse)
-    (ending_verse..beginning_verse).to_a.reverse.map {|n| verse(n)}.join("\n")
+    beginning_verse.downto(ending_verse).map {|n| verse(n)}.join("\n")
   end
 
   def bottles(n)
