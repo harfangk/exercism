9c32daf3ead3ebf51fa4323927de7fdb2a5664ab
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-04-14 02:35:07 +0900

    Duplication removed version
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -4,26 +4,32 @@ end
 
 class BeerSong
   def verse(n)
-    if n == 2
-      %Q(#{n} bottles of beer on the wall, #{n} bottles of beer.
-Take one down and pass it around, #{n - 1} bottle of beer on the wall.
-)
-    elsif n == 1
-      %Q(#{n} bottle of beer on the wall, #{n} bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-)
-    elsif n == 0
-      %Q(No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-)
-    else
-      %Q(#{n} bottles of beer on the wall, #{n} bottles of beer.
-Take one down and pass it around, #{n - 1} bottles of beer on the wall.
-)
-    end
+    first_verse = "#{bottles(n)} of beer on the wall, #{bottles(n)} of beer.".capitalize
+    second_verse = 
+      if n == 0
+        "Go to the store and buy some more, #{bottles(99)} of beer on the wall."
+      else
+        "Take #{correct_pronoun(n)} down and pass it around, #{bottles(n-1)} of beer on the wall."
+      end
+    "#{first_verse}\n#{second_verse}\n"
   end
 
   def verses(beginning_verse, ending_verse)
     (ending_verse..beginning_verse).to_a.reverse.map {|n| verse(n)}.join("\n")
+  end
+
+  def bottles(n)
+    case n
+    when 1
+      "1 bottle"
+    when 0
+      "no more bottles"
+    else
+      "#{n} bottles"
+    end
+  end
+
+  def correct_pronoun(n)
+    n == 1 ? "it" : "one"
   end
 end
