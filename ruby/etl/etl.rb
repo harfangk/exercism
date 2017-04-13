40367c11378fdf2b985de5292a7378579ca195ae
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-04-13 22:39:59 +0900

    Change to FP style
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,15 +1,10 @@
 class ETL
   def self.transform(old)
-
-    result = {}
-
-    old.each_pair do |k, v|
-      new_ks = v.map(&:downcase)
-      new_vs = [k] * v.length
-      new_hash = [new_ks, new_vs].transpose.to_h
-      result.merge!(new_hash)
+    old.reduce({}) do |memo, (k, v)|
+      ks = v.map(&:downcase)
+      vs = [k] * v.length
+      hash = [ks, vs].transpose.to_h
+      memo.merge!(hash)
     end
-
-    result
   end
 end
