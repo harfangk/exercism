52486800c414fc1d89c48949223b36e72272dbad
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-04-13 22:05:50 +0900

    Clean up code
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,30 +1,22 @@
 class PrimeFactors
   def self.for(n)
-    if n == 1
-      return []
-    end
-
     current_n = n
     factors = []
     i = 2
-
-    while i == 2
-      if current_n % i == 0
-        factors << i
-        current_n = current_n / i
-      else
-        i = 3
-      end
-    end
 
     while i <= current_n
       if current_n % i == 0
         factors << i
         current_n = current_n / i
       else
-        i += 2
+        if i == 2
+          i = 3
+        else
+          i += 2
+        end
       end
     end
+
     return factors
   end
 end
