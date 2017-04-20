bf04df038ab5657788cfeff21854a687743e2e2c
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-04-20 21:20:39 +0900

    Refactor variable names
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -3,37 +3,37 @@ module BookKeeping
 end
 
 class Clock
-  attr_reader :h, :m
+  attr_reader :hour, :minute
 
-  def initialize(h, m)
-    set_time(h, m)
+  def initialize(hour, minute)
+    set_time(hour, minute)
   end
 
-  def self.at(h, m)
-    Clock.new(h, m)
+  def self.at(hour, minute)
+    Clock.new(hour, minute)
   end
 
   def to_s
-    converted_h = @h.to_s.rjust(2, '0')
-    converted_m = @m.to_s.rjust(2, '0')
-    "#{converted_h}:#{converted_m}"
+    converted_hour = @hour.to_s.rjust(2, '0')
+    converted_minute = @minute.to_s.rjust(2, '0')
+    "#{converted_hour}:#{converted_minute}"
   end
 
-  def ==(y)
-    @h == y.h && @m == y.m
+  def ==(another_clock)
+    @hour == another_clock.hour && @minute == another_clock.minute
   end
 
-  def +(m)
-    set_time(@h, @m + m)
+  def +(minute)
+    set_time(@hour, @minute + minute)
     self
   end
 
   private
 
-  def set_time(h, m)
-    new_m = m % 60
-    new_h = (h + (m / 60)) % 24
-    @h = new_h
-    @m = new_m
+  def set_time(hour, minute)
+    new_minute = minute % 60
+    new_hour = (hour + (minute / 60)) % 24
+    @hour = new_hour
+    @minute = new_minute
   end
 end
