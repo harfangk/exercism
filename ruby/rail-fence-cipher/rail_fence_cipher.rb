3bf175e3d7366c72865c173a941ed2bc4a46d4c8
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-04-27 14:44:53 +0900

    Rewrite solution to build lookup table instead of processing by chunks
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- ruby/rail-fence-cipher/rail_fence_cipher.rb
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -1,99 +1,53 @@
 class RailFenceCipher
   VERSION = 1
 
-  def self.encode(plaintext, rail_rows)
-    if rail_rows <= 1 || plaintext.empty?
-      return plaintext
-    end
-
-    cycle = rail_rows * 2 - 2
-    plaintext_segments_by_cycle = plaintext.scan(/.{1,#{cycle}}/)
-    char_indices = RailFenceCipher.indices_for_each_rail_row(cycle)
-    ciphertext_by_rows = RailFenceCipher.split_plaintext_into_rows(plaintext_segments_by_cycle, char_indices, rail_rows)
-    ciphertext_by_rows.join
-  end
-
-  def self.decode(ciphertext, rail_rows)
-    if rail_rows <= 1 || ciphertext.empty?
-      return ciphertext
-    end
-
-    row_text_length_hash = RailFenceCipher.calculate_length_of_each_row(ciphertext, rail_rows)
-    segments = RailFenceCipher.split_ciphertext_to_each_row(ciphertext, row_text_length_hash)
-    RailFenceCipher.construct_plaintext_from_ciphertext_segments(segments)
-  end
-
-  def self.indices_for_each_rail_row(cycle)
-    all_indices =
-      (0...cycle).to_a.repeated_combination(2).select do |x, y| 
-        (x + y) % cycle == 0
-      end
-    all_indices.map(&:uniq)
-  end
-
-  def self.split_plaintext_into_rows(plaintext_segments_by_cycle, char_indices, rail_rows)
-    (0...rail_rows).map do |row|
-      ciphertext_for_row = ""
-      plaintext_segments_by_cycle.each do |segment|
-        char_indices[row].each do |char_index|
-          ciphertext_for_row += segment[char_index] if segment[char_index]
-        end
-      end
-      ciphertext_for_row
-    end
-  end
-
-  def self.calculate_length_of_each_row(ciphertext, rail_rows)
-    cycle = rail_rows * 2 - 2
-    complete_segments_count = ciphertext.length / cycle
-    last_segment_length = ciphertext.length % cycle
-
-    row_text_length_hash = Hash.new(0)
-    (0...rail_rows).each do |row|
-      if row == 0 || row == (cycle / 2) 
-        row_text_length_hash[row] = complete_segments_count
-      else
-        row_text_length_hash[row] = complete_segments_count * 2
-      end
-    end
-
-    (0...last_segment_length).each do |row|
-      if row <= (cycle / 2)
-        row_text_length_hash[row] += 1
-      else
-        row_text_length_hash[cycle - row] += 1
-      end
-    end
-
-    row_text_length_hash
-  end
-
-  def self.split_ciphertext_to_each_row(ciphertext, row_text_length_hash)
-    row_text_length_hash.values.reduce([]) do |memo, v|
-      segment = ciphertext.slice!(0, v)
-      memo << segment
-    end
-  end
-
-  def self.construct_plaintext_from_ciphertext_segments(segments)
-    plaintext = ""
-
-    while (segments.length > 0)
-      segments.each_with_index do |str, i|
-        if i != segments.length - 1
-          plaintext += str.slice!(0,1)
-        end
+  class << self
+    def encode(plaintext, rails_count)
+      if rails_count <= 1 || plaintext.empty?
+        return plaintext
       end
 
-      segments.reverse.each_with_index do |str, i|
-        if i != segments.length - 1
-          plaintext += str.slice!(0,1)
-        end
-      end
+      encoding_table = encoding_table(plaintext, rails_count)
 
-      segments.delete("")
+      plaintext.chars.each_with_index.map do |c, i|
+        ciphertext_index = encoding_table[i]
+        plaintext[ciphertext_index]
+      end.join
     end
 
-    plaintext
+    def decode(ciphertext, rails_count)
+      if rails_count <= 1 || ciphertext.empty?
+        return ciphertext
+      end
+
+      decoding_table = decoding_table(ciphertext, rails_count)
+
+      ciphertext.chars.each_with_index.map do |c, i|
+        plaintext_index = decoding_table[i]
+        ciphertext[plaintext_index]
+      end.join
+    end
+
+    private
+
+    def encoding_table(str, rails_count)
+      cycle = cycle(rails_count)
+      char_indices_per_rail = Array.new(rails_count) { Array.new }
+      (0...str.length).each do |char_index|
+        index_in_cycle = char_index % [1, cycle].max
+        rail_row = index_in_cycle < rails_count ? index_in_cycle : cycle - index_in_cycle
+        char_indices_per_rail[rail_row] << char_index
+      end
+      encoding_table_hash = (0...str.length).to_a.zip(char_indices_per_rail.flatten).to_h
+      encoding_table_hash 
+    end
+
+    def decoding_table(str, rails_count)
+      encoding_table(str, rails_count).invert
+    end
+
+    def cycle(rails_count)
+      rails_count * 2 - 2
+    end
   end
 end
