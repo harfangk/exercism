6a6dbd2bf106ead2e41299e4dcd9400247617f6c
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-09-17 17:04:39 +0900

    Initial solution to elixir rotational cipher exercise
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,5 +8,25 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> String.to_charlist()
+    |> Enum.map(fn x -> shift_english_alphabets(x, shift) end)
+    |> List.to_string()
   end
+
+  defp shift_english_alphabets(c, shift) when (c <= 122 and c >= 97) do
+    c
+    |> Kernel.-(97)
+    |> Kernel.+(shift)
+    |> Kernel.rem(26)
+    |> Kernel.+(97)
+  end
+  defp shift_english_alphabets(c, shift) when (c <= 90 and c >= 65) do
+    c
+    |> Kernel.-(65)
+    |> Kernel.+(shift)
+    |> Kernel.rem(26)
+    |> Kernel.+(65)
+  end
+  defp shift_english_alphabets(c, shift), do: c
 end
