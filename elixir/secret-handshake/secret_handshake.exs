e6d50cf2d2dd3457428aac4c8334f173293c92b2
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-09-17 11:49:59 +0900

    Initial solution to Elixir secret handshake
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,4 +1,6 @@
 defmodule SecretHandshake do
+  use Bitwise
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,6 +17,28 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-  end
-end
+    code
+    |> get_five_least_significant_bits_of()
+    |> decipher_code()
+   end
 
+   defp get_five_least_significant_bits_of(code) do
+     rem(code, 32)
+   end
+
+  defp decipher_code(code) do
+    table = [
+      {1, "wink"},
+      {2, "double blink"},
+      {4, "close your eyes"},
+      {8, "jump"}
+    ]
+    has_event? = fn(x) -> ((code &&& x) == x) end
+    actions = for {k, v} <- table, has_event?.(k), into: [], do: v
+    if (code &&& 16) == 16 do
+      Enum.reverse(actions)
+    else
+      actions
+    end
+  end 
+end
